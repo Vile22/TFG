07b3f659dfd348dd580e12c98839eaa798419082
Vile22 <jorgevilellacarcases@gmail.com>
2022-09-27 10:17:58 +0200

    Version inicial

--- prueba_excel.py
+++ prueba_excel.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
 import pandas as pd
 import openpyxl
-#Hola
+
 df = pd.DataFrame(columns=['Tiempo'])
 for i in range(1,16):
     df = df.append({'Tiempo': 4}, ignore_index=True)
--- talker_excel.py
+++ talker_excel.py
@@ -1,23 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Float64
+import pandas as pd
+from std_msgs.msg import Float32
+from random import randint
 
 
 def time_pub():
-    pub = rospy.Publisher('Time', Float64, queue_size=100)
+    global df
+    pub = rospy.Publisher('Tiempo', Float32, queue_size=1000)
     rospy.init_node('time_pub_node', anonymous = True)
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(3)
 
     while not rospy.is_shutdown():
-        time = Float64(rospy.get_time)
-        rospy.loginfo(time)
-        pub.publish(time)
+        var = float(randint(0,4))
+        rospy.loginfo(var)
+        pub.publish(var)
+        df = df.append({'Tiempo': var}, ignore_index=True)
         rate.sleep()
 
+df = pd.DataFrame(columns = ['Tiempo'])
 
 if __name__ == '__main__':
     try:
         time_pub()
+        if rospy.is_shutdown():
+            df.to_excel('pandas_to_excel.xlsx', sheet_name = 'Nueva hoja') 
     except rospy.ROSInterruptException:
         pass
