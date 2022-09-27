bf685b02fb7f407fec3dcae27f0f6ff0585fd24e
Vile22 <jorgevilellacarcases@gmail.com>
2022-09-27 09:39:02 +0200

    Versión inicial

---
+++ prueba_excel.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import pandas as pd
+import openpyxl
+
+df = pd.DataFrame(columns=['Tiempo'])
+for i in range(1,16):
+    df = df.append({'Tiempo': 4}, ignore_index=True)
+
+df.to_excel('pandas_to_excel.xlsx', sheet_name = 'Nueva hoja')
